819b7476f1a11a99c3a1a17175324119905dd496
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2015-09-12 18:25:11 -0600

    Add jshint task

---
+++ tasks/config/jshint.js
@@ -0,0 +1,26 @@
+/**
+ * Validate files with JSHint
+ *
+ * ---------------------------------------------------------------
+ *
+ * For usage docs see:
+ *    https://github.com/gruntjs/grunt-contrib-JSHint
+ */
+
+module.exports = function(grunt) {
+
+    grunt.config.set('jshint', {
+        options: {
+            jshintrc: '.jshintrc',
+            reporter: require('jshint-stylish')
+        },
+        frontend: {
+            src: ['assets/scripts/**/*.js']
+        },
+        backend: {
+            src: ['api/**/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+};
