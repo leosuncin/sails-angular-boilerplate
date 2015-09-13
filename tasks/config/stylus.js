13bc537b821c0b88af1637aea78da7d128b06c67
Jaime Leonardo Suncin Cruz <leosuncin@gmail.com>
2015-09-12 21:12:05 -0600

    Add stylus task

---
+++ tasks/config/stylus.js
@@ -0,0 +1,31 @@
+/**
+ * Compiles LESS files into CSS.
+ *
+ * ---------------------------------------------------------------
+ *
+ * Only the `assets/styles/importer.stylus` is compiled.
+ * This allows you to control the ordering yourself, i.e. import your
+ * dependencies, mixins, variables, resets, etc. before other stylesheets)
+ *
+ * For usage docs see:
+ * 		https://github.com/gruntjs/grunt-contrib-stylus
+ */
+module.exports = function(grunt) {
+
+	grunt.config.set('stylus', {
+		dev: {
+      options: {
+        compress: false
+      },
+			files: [{
+				expand: true,
+				cwd: 'assets/styles/',
+				src: ['importer.styl'],
+				dest: '.tmp/public/styles/',
+				ext: '.css'
+			}]
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-stylus');
+};
